cba6d1f4756fdb9b0131fa2f2ffb37a55b8c4b34
rin <prettysunshine468@gmail.com>
2022-03-17 16:03:48 +0600

    add minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:expandable/expandable.dart';
 
 enum statusTitle {
   latest,
@@ -15,42 +15,40 @@ TextStyle appTextStyle(double fontSize) {
 }
 
 Text StatusTitle(statusTitle taskStatus) {
+  final String status;
+  final Color color;
   if (taskStatus == statusTitle.latest) {
-    return Text(
-      "Новое",
-      style: TextStyle(
-        color: Colors.green,
-        fontSize: 12,
-        fontFamily: "fonts/Lato-Regular.ttf",
-      ),
-    );
+    status = "Новое";
+    color = Colors.green;
   } else if (taskStatus == statusTitle.inProgress) {
-    return const Text(
-      "В прогрессе",
-      style: TextStyle(
-        color: Colors.blue,
-        fontSize: 12,
-        fontFamily: "fonts/Lato-Regular.ttf",
-      ),
-    );
+    status = "В прогрессе";
+    color = Colors.blue;
   } else {
-    return Text(
-      "Сделано",
-      style: TextStyle(
-        color: Colors.grey,
-        fontSize: 12,
-        fontFamily: "fonts/Lato-Regular.ttf",
-      ),
-    );
+    status = "Сделано";
+    color = Colors.grey;
   }
+  return Text(
+    status,
+    style: TextStyle(
+      color: color,
+      fontSize: 12,
+      fontFamily: "fonts/Lato-Regular.ttf",
+    ),
+  );
 }
 
 void main() {
-  runApp(const TasksListPage());
+  runApp(TasksListPage());
 }
 
 class TasksListPage extends StatelessWidget {
-  const TasksListPage({Key? key}) : super(key: key);
+  List<Widget> tasks = [
+    TaskCard(
+        taskTopic: "Поменять лампочку",
+        taskDescription:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        taskDeadline: "8ч."),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -59,39 +57,40 @@ class TasksListPage extends StatelessWidget {
       home: Scaffold(
         body: Column(
           children: <Widget>[
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.baseline,
-              textBaseline: TextBaseline.ideographic,
-              children: <Widget>[
-                Container(
-                  width: 140,
-                  height: 26,
-                  margin: EdgeInsets.fromLTRB(22, 60, 185, 40),
-                  child: Text(
-                    "Список задач",
-                    style: appTextStyle(20),
-                  ),
+            Container(
+              width: 150,
+              height: 26,
+              margin: const EdgeInsets.fromLTRB(0, 60, 185, 40),
+              child: const Text(
+                "Список задач",
+                style: TextStyle(
+                  fontFamily: "fonts/Lato-Regular.ttf",
+                  fontWeight: FontWeight.w500,
+                  fontSize: 22,
                 ),
-                Container(
-                  width: 17,
-                  height: 17,
-                  child: Icon(
-                    Icons.edit,
-                    size: 17,
-                  ),
-                ),
-              ],
+              ),
             ),
             Container(
-              width: 280,
-              height: 15,
-              margin: EdgeInsets.only(bottom: 30),
-              child: Text(
-                "Placeholder for search bar",
-              ),
-            ), //SearchBar
+                width: 350,
+                height: 30,
+                margin: const EdgeInsets.fromLTRB(22, 0, 22, 30),
+                child: const Expanded(
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(left: 320),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                )), //SearchBar
             Container(
-              margin: EdgeInsets.symmetric(horizontal: 15),
+              margin: const EdgeInsets.symmetric(horizontal: 15),
               height: 40,
               child: Row(
                 children: <Widget>[
@@ -122,11 +121,34 @@ class TasksListPage extends StatelessWidget {
                 ],
               ),
             ), //SortBy
-            TaskCard(
-                taskTopic: "Поменять лампочку",
-                taskDescription:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                taskDeadline: "8ч.")
+            Column(
+              children: tasks,
+            ),
+            Container(
+              width: 110,
+              height: 30,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Добавить",
+                    style: appTextStyle(14),
+                  ),
+                  const Icon(
+                    Icons.add_box_outlined,
+                    size: 20,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -158,7 +180,7 @@ class SortByCard extends StatelessWidget {
       ),
       decoration: BoxDecoration(
         border: Border.all(
-          color: Colors.black,
+          color: Colors.grey,
         ),
         borderRadius: const BorderRadius.all(
           Radius.circular(10),
@@ -169,9 +191,10 @@ class SortByCard extends StatelessWidget {
 }
 
 class TaskCard extends StatelessWidget {
-  TaskCard({required this.taskTopic,
-    required this.taskDescription,
-    required this.taskDeadline});
+  TaskCard(
+      {required this.taskTopic,
+      required this.taskDescription,
+      required this.taskDeadline});
 
   final String taskTopic;
   final String taskDescription;
@@ -184,7 +207,7 @@ class TaskCard extends StatelessWidget {
       padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
         border: Border.all(
-          color: Colors.black,
+          color: Colors.grey,
         ),
         borderRadius: const BorderRadius.all(
           Radius.circular(10),
@@ -201,9 +224,10 @@ class TaskCard extends StatelessWidget {
                 style: appTextStyle(14),
               ),
               Container(
-                margin: const EdgeInsets.only(left: 80),
+                margin: const EdgeInsets.only(left: 88),
                 child: Text(
                   "срок: $taskDeadline",
+                  style: appTextStyle(14),
                 ),
               ),
             ],
@@ -216,32 +240,32 @@ class TaskCard extends StatelessWidget {
             ),
           ),
           Row(
-              children: <Widget>[
-                Row(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  StatusTitle(statusTitle.inProgress),
+                  const Icon(
+                    Icons.keyboard_arrow_down_sharp,
+                    size: 30,
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 125),
+                child: Row(
                   children: <Widget>[
-                    StatusTitle(statusTitle.inProgress),
-                    Icon(
+                    Text(
+                      "Развернуть",
+                      style: appTextStyle(12),
+                    ),
+                    const Icon(
                       Icons.keyboard_arrow_down_sharp,
                       size: 30,
                     ),
                   ],
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 125),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        "Развернуть",
-                        style: appTextStyle(12),
-                      ),
-                      Icon(
-                        Icons.keyboard_arrow_down_sharp,
-                        size: 30,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
+            ],
           ),
         ],
       ),
